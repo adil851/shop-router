65e00f71b7c55ab14d869a8f0360927b0bf8e8b5
adil851 <59330983+adil851@users.noreply.github.com>
2020-07-22 13:25:40 -0700

    Initialize project using Create React App

---
+++ src/utils/social.js
@@ -0,0 +1,3 @@
+export const facebookURL = "https://www.facebook.com/adil.butt.96780/";
+export const githubURL = "https://github.com/adil851/"; 
+export const linkedInURL = "https://www.linkedin.com/in/adil-butt-b7617311a/";
